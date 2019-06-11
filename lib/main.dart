95a7fb24f5bc6561a648441971862b38b726c13b
jkyony <jkyony@gmail.com>
2019-06-11 13:31:10 +0200

    google login

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:recipes_app/app.dart';
+import 'package:recipes_app/state_widget.dart';
 
-void main() => runApp(
-  new RecipesApp(),
-);
+// void main() => runApp(
+//   new RecipesApp(),
+// );
+
+void main() => runApp(new StateWidget(
+  child:  new RecipesApp(),
+));
